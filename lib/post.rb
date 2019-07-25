62a0971ba8d6f9837bae846b4ff66ab7169947ac
Anthony Molina <antonio-molina@att.net>
2019-07-25 21:16:45 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,26 @@
+class Post
+  
+  attr_accessor :title, :author 
+  
+  def initialize(title) 
+    @title = title
+  end 
+  
+  def author_name 
+    if @author == nil 
+      return nil 
+    else 
+      @author.name 
+    end 
+  end 
+  
+  def self.all 
+    post
+  end 
+end 
+
+  
+  
+  
+  
+  
---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song 
+  attr_accessor :name, :artist 
+  def initialize(name) 
+    @name = name 
+  end 
+  
+  def artist_name 
+    if @artist == nil 
+      return nil
+    else 
+      @artist.name 
+    end 
+  end 
+  
+  def self.all 
+    @@song_count
+  end 
+  
+  
+end 
